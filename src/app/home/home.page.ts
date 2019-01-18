93f0ccf0382bde0c652342dd3af5b1003cef3acd
cs_tarc <cs_tarc@cs-tarcs-iMac.local>
2019-01-18 13:45:20 +0800

    Test ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public actionSheet: ActionSheetController){}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheet.create({
+      header: 'Action Sheet',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: 'trash',
+          handler: () => {
+            console.log("You Delete me");
+          }
+        },
+        {
+          text: 'Add',
+          role: 'destructive',
+          icon: 'add',
+          handler: () => {
+            console.log("You add me");
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
+  }
 
 }
